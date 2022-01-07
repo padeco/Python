51b4b20eb2f06629fba10ebff6c58a3addf6b29b
ManoelTorres <54108258+padeco@users.noreply.github.com>
2022-01-07 16:09:28 -0300

    filter map reduce
    16:09

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import os
-import time
+class Animal:
+    def __init__(self, especie, idade):
+        self.especie = especie
+        self.idade = idade
+        
+        
+class Cachorro(Animal):
+    def falar(self):
+        print('au-au')
+        
+class Humano(Animal):
+    def falar(self):
+        print('Merda')
+        
+        
+        
+a1 = Cachorro('labrador', 15)
+a2 = Humano('Jose', 75)
 
-def clearConsole():
-    command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-        command = 'cls'
-    os.system(command)
-resposta = 0
-def main():
-    
-    clearConsole()
-    print('''
-    ___________                                 
-    \__    ___/_________________   ____   ______
-      |    | /  _ \_  __ \_  __ \_/ __ \ /  ___/
-      |    |(  <_> )  | \/|  | \/\  ___/ \___ \ 
-      |____| \____/|__|   |__|    \___  >____  >
-                                      \/     \/ ''')
-    print('Selecione as opces para navegar entre os exercicios:\n')
-    print('escolha a aula:')
-    resposta = input()
-    #teste = ('ex' + resposta + ())
+a2.falar()
+a1.falar()
