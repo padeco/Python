dafcebc038ced394547cc52c60c38c09bb868411
ManoelTorres <54108258+padeco@users.noreply.github.com>
2022-01-11 15:32:15 -0300

    11/01/2022
    Conseguimos depois de muita luta dor e sofrimento.

--- EXERCICIOS/ex4_kumulus/main.py
+++ EXERCICIOS/ex4_kumulus/main.py
@@ -1,27 +1,28 @@
 from functools import reduce
 from pprint import pprint
-import csv
-import sys
+import time
 
 _print = print
 print = pprint
 
-arquivo_completo = ('C:\\Users\\manoel.torres\\OneDrive - Kumulus\\Documents\\GitHub\\Python\\EXERCICIOS\\ex4_kumulus\\reccrimepfa-210902-120414.csv')
-
-with open(arquivo_completo, 'r') as arquivo:
+def data(data):
+    '''funcao que faz a comparacao entre as datas'''
+    date1 = "01/01/2012"
+    date2 = data['12 months ending']
+    newdate1 = time.strptime(date1, "%d/%m/%Y")
+    newdate2 = time.strptime(date2, "%d/%m/%Y")
+    if newdate1 <= newdate2:
+        return data['12 months ending']
     
-    arquivo_csv = iter(csv.DictReader(arquivo, delimiter=','))
-    #for linha in arquivo_csv:
-    print(type(arquivo_csv))
-    print(sys.getsizeof(arquivo_csv))   
-         
-    anos = (
-        map(lambda x: x['12 months ending'],
-            filter(
-            lambda x: x ['12 months ending']== '31/03/2013', 
-               arquivo_csv
-               )       )
-        )
+arquivo_completo = ('C:\\Users\\manoel.torres\\OneDrive - Kumulus\\Documents\\GitHub\\Python\\EXERCICIOS\\ex4_kumulus\\reccrimepfa-210902-120414.csv')
+    
+linhas = (line for line in open(arquivo_completo))
+lista_linha = (s.rstrip().split(",") for s in linhas)
+colunas = next(lista_linha)
+para_dicio = (dict(zip(colunas, data)) for data in lista_linha)
 
-    print(list(anos))
-    print(sys.getsizeof(anos))
+crimes_dicio = map(lambda x:x['Rolling year total number of offences'],filter(data,para_dicio))
+
+result_soma = reduce(lambda soma, crime: soma + int(crime), crimes_dicio, 0 )
+
+print(f'Resultado da soma dos crimes a partir de 2012 é {result_soma}')
